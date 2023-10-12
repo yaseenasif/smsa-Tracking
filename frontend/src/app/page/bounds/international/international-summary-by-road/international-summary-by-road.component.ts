8f18a31410b5f01d431da627d148a8e4534a8575
Shahzad <shahzad.iqbal.developer@gmail.com>
2023-10-12 20:40:35 +0500

    summary WIP

--- frontend/src/app/page/bounds/international/international-summary-by-road/international-summary-by-road.component.ts
+++ frontend/src/app/page/bounds/international/international-summary-by-road/international-summary-by-road.component.ts
@@ -7,6 +7,13 @@ import { MenuItem } from 'primeng/api';
   styleUrls: ['./international-summary-by-road.component.scss']
 })
 export class InternationalSummaryByRoadComponent {
+  name!:string;
+  location!:Location[];
+  status!:Status[];
+  selectedStatus!:Status;
+  selectedLocation!:Location;
+  fromDate: Date | undefined;
+  toDate: Date | undefined;
   constructor() { }
   products:any=[{preAlertNumber:"320012345678",vehicalNumber:"N/A",vehiclaType:"N/A",mode:"Courier",origin:"UAE",originPort:"DXB",destinationPort:"DMM",weight:"2000",totalShipment:"200",pallets:"20",bags:"95",etd:"##########",eta:"#########",ata:"N/A",status:"Departure"},
   {preAlertNumber:"320012345678",vehicalNumber:"N/A",vehiclaType:"N/A",mode:"Courier",origin:"UAE",originPort:"DXB",destinationPort:"DMM",weight:"2000",totalShipment:"200",pallets:"20",bags:"95",etd:"##########",eta:"#########",ata:"N/A",status:"Departure"},
@@ -24,5 +31,33 @@ export class InternationalSummaryByRoadComponent {
 
   ngOnInit() {
       this.items = [{ label: 'International Summary By Road'}];
+      this.location=[
+        {
+          locationName:"In bound",
+          id:1
+        },
+        {
+          locationName:"Out bound",
+          id:2
+        }
+      ]
+
+      this.status=[
+        {
+          status:"Open"
+        },
+        {
+          status:"Close"
+        }
+      ]
   }
+}
+interface Location{
+  locationName:string,
+  id:number
+}
+
+
+interface Status{
+  status:string
 }
