d0d813e9f930a82624bcdf228862a02f7f0cf3cd
DELL <aliazlanbagga@gmail.com>
2023-10-25 20:10:32 +0500

    driver work

--- frontend/src/app/page/driver/driver-list/driver-list.component.ts
+++ frontend/src/app/page/driver/driver-list/driver-list.component.ts
@@ -1,30 +1,52 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuItem } from 'primeng/api';
+import { MenuItem, MessageService } from 'primeng/api';
+import { DriverService } from '../service/driver.service';
+import { Driver } from 'src/app/model/Driver';
+import { PaginatedResponse } from 'src/app/model/PaginatedResponse';
 
 @Component({
   selector: 'app-driver-list',
   templateUrl: './driver-list.component.html',
-  styleUrls: ['./driver-list.component.scss']
+  styleUrls: ['./driver-list.component.scss'],
+  providers:[MessageService]
 })
 export class DriverListComponent implements OnInit {
-
-  constructor() { }
-  products:any=[{name:"Demo",contactNumber:"Demo",referenceNumber:"Demo"},
-  {name:"Demo",contactNumber:"Demo",referenceNumber:"Demo"},
-  {name:"Demo",contactNumber:"Demo",referenceNumber:"Demo"},
-  {name:"Demo",contactNumber:"Demo",referenceNumber:"Demo"},
-  {name:"Demo",contactNumber:"Demo",referenceNumber:"Demo"},
-  {name:"Demo",contactNumber:"Demo",referenceNumber:"Demo"},
-  {name:"Demo",contactNumber:"Demo",referenceNumber:"Demo"},
-  {name:"Demo",contactNumber:"Demo",referenceNumber:"Demo"},
-  {name:"Demo",contactNumber:"Demo",referenceNumber:"Demo"},
-  {name:"Demo",contactNumber:"Demo",referenceNumber:"Demo"},];
+  visible!: boolean;
+  dId!:number;
   items: MenuItem[] | undefined;
+  drivers!:Driver[]
 
+  constructor(private driverService:DriverService,private messageService:MessageService) { }
+  
  
 
   ngOnInit() {
       this.items = [{ label: 'Driver List'}];
+      this.getAllDriver();
+  }
+  
+  getAllDriver(){
+   this.driverService.getAllDriver().subscribe((res:PaginatedResponse<Driver>)=>{
+    debugger
+    this.drivers=res.content.filter((el:Driver)=>el.status); 
+    console.log( this.drivers);
+    
+   },error=>{})
+  }
+
+  deleteDriverByID(id:number){
+    this.driverService.deleteDriverByID(id).subscribe((res:Driver)=>{
+      this.visible = false;
+      this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Driver is deleted on id '+res!.id!.toString()});
+      this.getAllDriver();
+    },error=>{
+      
+    });
+   }
+
+  showDialog(id:number) {
+    this.dId=id;
+    this.visible = true;
   }
 
 }
--- frontend/src/app/page/driver/update-driver/update-driver.component.ts
+++ frontend/src/app/page/driver/update-driver/update-driver.component.ts
@@ -1,21 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuItem } from 'primeng/api';
+import { MenuItem, MessageService } from 'primeng/api';
+import { DriverService } from '../service/driver.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Driver } from 'src/app/model/Driver';
 
 @Component({
   selector: 'app-update-driver',
   templateUrl: './update-driver.component.html',
-  styleUrls: ['./update-driver.component.scss']
+  styleUrls: ['./update-driver.component.scss'],
+  providers:[MessageService]
 })
 export class UpdateDriverComponent implements OnInit {
 
   items: MenuItem[] | undefined;
+  driver:Driver={
+    id: null,
+    contactNumber: null,
+    name: null,
+    referenceNumber: null,
+    status: null
+  }
+  dId!:number;
+ 
+  constructor(private route: ActivatedRoute,
+    private driverService:DriverService,
+    private messageService: MessageService,
+    private router: Router) { }
 
-  constructor() { }
-  name!:string;
-  contactNumber!:string;
-  referenceNumber!:string;
   ngOnInit(): void {
+    this.dId = +this.route.snapshot.paramMap.get('id')!;
     this.items = [{ label: 'Driver List',routerLink:'/driver'},{ label: 'Edit Driver'}];
+    this.getDriverById();
+  }
+
+  getDriverById(){
+    this.driverService.getDriverByID(this.dId).subscribe((res:Driver)=>{
+     this.driver=res;
+    },error=>{
+     this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Can not find Driver on this id'});
+    })
+   }
+
+   onSubmit() {
+    this.driverService.updateDriverById(this.dId,this.driver).subscribe(res=>{
+      this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Driver is updated on id '+res.id});
+      setTimeout(() => {
+        this.router.navigate(['/driver']);
+      },800);
+    },error=>{
+      this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Driver is not updated'});
+    })  
   }
 
 }
