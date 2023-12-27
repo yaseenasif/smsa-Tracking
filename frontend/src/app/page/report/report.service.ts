27826994e3343840b0dbcb671e2fb5f914ce01c9
DELL <aliazlanbagga@gmail.com>
2023-12-28 00:02:48 +0500

    report service work on IARS

--- frontend/src/app/page/report/report.service.ts
+++ frontend/src/app/page/report/report.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { InternationalAirReportPerformance } from 'src/app/model/InternationalAirReportPerformance';
+import { InternationalAirReportStatus } from 'src/app/model/InternationalAirReportStatus';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -13,5 +14,8 @@ export class ReportService {
   url=environment.baseurl;
   getInternationalAirReportPerformance():Observable<InternationalAirReportPerformance[]>{
     return this.http.get<InternationalAirReportPerformance[]>(this.url.concat('/int-air-report-performance'));
+  }
+  getInternationalAirReportStatus():Observable<InternationalAirReportStatus[]>{
+    return this.http.get<InternationalAirReportStatus[]>(this.url.concat('/int-air-report-status'));
   }
 }
